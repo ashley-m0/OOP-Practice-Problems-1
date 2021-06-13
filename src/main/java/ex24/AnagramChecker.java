d2030aba0210d6e186ccc8fc6c122a7e1f55a59e
ashley-m0 <ashley.mojica@knights.ucf.edu>
2021-06-13 09:47:43 -0400

    Initial commit

---
+++ src/main/java/ex24/AnagramChecker.java
@@ -0,0 +1,30 @@
+package ex24;
+
+public class AnagramChecker {
+
+    private String anagram1;
+    private String anagram2;
+
+    public AnagramCheck(String anagram1, String anagram2){
+        this.anagram1 = anagram1;
+        this.anagram2 = anagram2;
+    }
+
+    public void setAnagram1(String anagram1){
+        this.anagram1 = anagram1;
+    }
+
+    public void setAnagram2(String anagram2){
+        this.anagram2 = anagram2;
+    }
+
+    public String getAnagram1(){
+        return anagram1;
+    }
+
+    public String getAnagram2(){
+        return anagram2;
+    }
+
+    public
+}
